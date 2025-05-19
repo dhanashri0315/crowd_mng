53a2b70c3efda5d079acf27937a6beae1607705c
dhanashri0315 <dhanashriwarake2003@gmail.com>
2025-05-19 12:17:46 +0530

    Delete 1_Detection_Results.py

--- 1_Detection_Results.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Crowd Detection Results")
-st.write("This page shows detected individuals in the video.")
-
-# Placeholder for displaying video results
-st.video("outputs/detected_video.mp4")
